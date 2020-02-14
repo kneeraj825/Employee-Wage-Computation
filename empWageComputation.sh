09beb60b59a8f9b760cfc1cb326ee895f19f955a
kneeraj825 <kneeraj825@gmail.com>
2020-02-14 17:50:48 +0530

    Added:Part time wage using case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,19 +1,24 @@
 #!/bin/bash -x
+
 #cheking the employee is for part time and calculating  the salary 
 is_Part_Time=0
 is_Full_Time=1
 employeeCheck=$(( RANDOM%2 ))
 empWagePerHours=20
-if (( $is_Full_Time == $employeeCheck ))
-then
+
+case $employeeCheck in
+$is_Full_Time)
 	empHours=8
 #calculating the full time salary of the employee
 	fullTimesalary=$(( $empWagePerHours*$empHours ))
 	echo "full time salary of the employee is: $fullTimesalary"
-else
+;;
+$is_Part_Time)
 	empHours=4
 #calculating the part time salary of the employee
 	partTimesalary=$(( $empWagePerHours*$empHours ))
 	echo "part  time salary of the employee is: $partTimesalary"
-fi
+;;
+*)
+esac
 
